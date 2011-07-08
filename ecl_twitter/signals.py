ca1176d88ff95049334776e6e344aecc1e2614ce
Dan Loewenherz <dloewenherz@gmail.com>
2011-07-08 15:12:32 -0700

    remove ecl_twitter (moving to a separate app)

--- ecl_twitter/signals.py
+++
@@ -1,6 +0,0 @@
-from django.dispatch import Signal
-
-twitter_auth_started = Signal(providing_args=[])
-twitter_auth_completed = Signal(providing_args=['access_token', \
-        'access_token_secret', 'username', 'id'])
-

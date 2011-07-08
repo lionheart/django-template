ba1ebb3a005687a0d8e5dcf882c18360eaaaef3d
Dan Loewenherz <dloewenherz@gmail.com>
2011-07-08 15:14:36 -0700

    remove bad imports

--- app/views.py
+++ app/views.py
@@ -1,19 +1,4 @@
 import logging
-import random
-
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse
-from django.http import *
-from django.shortcuts import render, get_object_or_404
-from django.db.models import Q
-from django.views.decorators.csrf import csrf_exempt
-
-from decorators import unauthenticated_users_only
-from forms import *
-
-from goodiebag.utils import twitter, facebook
 
 logger = logging.getLogger(__name__)
 

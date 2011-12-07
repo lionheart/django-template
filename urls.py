db2265930f6f512903b3a900890df56bd54131ba
Dan Loewenherz <dloewenherz@gmail.com>
2011-12-07 15:43:48 -0800

    fix up urls.py

--- urls.py
+++ urls.py
@@ -1,9 +1,26 @@
 from django.conf.urls.defaults import patterns, include, url
 
 from django.contrib import admin
+from ecl_django.utils import simple_url
+from ecl_django.utils import template_url
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'home', name='home'),
 )
+
+# XXX Uncomment for Facebook integration
+# import ecl_facebook
+#
+# urlpatterns += patterns('ecl_facebook.views',
+#     url(r'oauth/facebook/begin$', 'oauth_facebook_begin', name='oauth-facebook-begin'),
+# )
+
+if settings.DEBUG:
+    urlpatterns += (
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT }),
+        )
 

d4ceef35b7b6bd85ea992da11e427f529aa6b028
Dan Loewenherz <dloewenherz@gmail.com>
2011-12-26 16:47:03 -0800

    reorder production configuration

--- conf/production.py
+++ conf/production.py
@@ -3,13 +3,6 @@
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -18,6 +11,13 @@ DATABASES = {
         'PASSWORD': 'goodiebag',
         'HOST': '127.0.0.1',
         'PORT': '',
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
     }
 }
 

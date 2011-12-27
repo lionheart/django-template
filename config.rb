2aef4e145acf5e32f858a350dd62a39137dfa717
Dan Loewenherz <dloewenherz@gmail.com>
2011-12-26 20:40:13 -0800

    add config

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "media/css"
+sass_dir = "media/css"
+images_dir = "media/images"
+javascripts_dir = "javascripts"
+line_comments = false
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true

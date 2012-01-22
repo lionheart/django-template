97c14718adc4146b724e7ed424eb9afd8aedf90c
Dan Loewenherz <dloewenherz@gmail.com>
2012-01-21 17:03:57 -0800

    prevent iframe takeovers

--- media/js/main.js
+++ media/js/main.js
@@ -1,3 +1,8 @@
+
+window.onbeforeunload = function () {
+    window.top.location = "/204";
+}
+
 (function(window) {
     if (window.location !== window.top.location) {
         window.top.location = window.location;
--- urls.py
+++ urls.py
@@ -1,15 +1,18 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
+from django.http import HttpResponse
 
 from ecl_django.utils import simple_url
 from ecl_django.utils import template_url
 from ecl_django.utils import home_url
+from ecl_django.utils import status_204
 
 admin.autodiscover()
 
 urlpatterns = patterns('app.views',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^204$', status_204),
     home_url('home'),
 )
 

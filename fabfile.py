56726c9b21409618f7fda01d05263e4e6ccfd61c
Dan Loewenherz <dloewenherz@gmail.com>
2012-02-18 11:11:45 -0800

    clean up fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,49 +2,36 @@ from fabric import colors
 from fabric.api import *
 from fabric.contrib.project import *
 
-env.user = 'ubuntu'
 env.app = 'goodiebag'
 env.dest = "/var/www/%(app)s" % env
-
-env.roledefs['app'] = ['web3.elmcitylabs.com']
-HOME = os.getenv('HOME')
-env.key_filename = [os.path.join(HOME, ".ssh/goodiebag-web-servers.pem")]
+env.use_ssh_config = True
 
 def production():
     env.label = "production"
 
-@roles('app')
 def reload_gunicorn():
     sudo("kill -HUP `cat /var/run/gunicorn/%(app)s.pid`" % env)
 
-@roles('app')
 def start_gunicorn():
     with cd(env.dest):
         sudo("gunicorn_django -c gunicorn/%(label)s.py" % env)
 
-@roles('app')
 def stop_gunicorn():
     sudo("kill `cat /var/run/gunicorn/%(app)s.pid`" % env)
 
-@roles('app')
 def reload_nginx():
     print(colors.yellow("Reloading nginx"))
     sudo("kill -HUP `cat /var/run/nginx.pid`")
 
-@roles('app')
 def shutdown_nginx():
     print(colors.red("Shutting down nginx"))
     sudo("kill -QUIT `cat /var/run/nginx.pid`")
 
-@roles('app')
 def deploy():
     rsync()
     reload_gunicorn()
 
-@roles('app')
 def rsync():
     print(colors.yellow("Deploying sources to %(host)s." % env))
-    key_string = " -i".join(env.key_filename)
-    env.key_string = key_string
-    local("""rsync --rsh='ssh -i %(key_string)s' -FPOuhimrtyz . %(user)s@%(host)s:%(dest)s""" % env)
+    rsync_project(env.dest, ".", extra_opts="-FPOuhimrtyz")
 

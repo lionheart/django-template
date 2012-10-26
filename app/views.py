deb4030f6f54fb03dfb57ca37efbeb222d80aa3e
Dan Loewenherz <dloewenherz@gmail.com>
2012-10-25 22:16:33 -0700

    change ecl_django to plus

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import logging
 
 from django.shortcuts import get_object_or_404
 
-from ecl_django.decorators import render
+from plus.decorators import render
 
 logger = logging.getLogger(__name__)
 

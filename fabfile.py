eb4f15160a2fa5e1b0e2702adf3b4900c42ea1fa
Dan Loewenherz <dloewenherz@gmail.com>
2012-11-12 15:50:00 -0800

    add provisioning script, update gunicorn configurations

--- fabfile.py
+++ fabfile.py
@@ -11,13 +11,22 @@ env.use_ssh_config = True
 def reload_processes():
     sudo("kill -HUP `cat /tmp/%(app)s.pid`" % env)
 
-def deploy():
+def sync():
     repo = git.Repo(".")
     sha = repo.head.commit.hexsha
     with cd(env.dest):
         run("git fetch --all")
         run("git checkout {} -f".format(sha))
 
+    if "production" in env.host_string:
+        with cd(env.dest):
+            run("compass compile")
+
+            with prefix(". /home/ubuntu/environments/%(app)s/bin/activate" % env):
+                run("%(dest)s/manage.py syncmedia" % env)
+
+def deploy():
+    sync()
     reload_processes()
 
 def link_files():
@@ -33,23 +42,12 @@ def link_files():
         sudo("rm -f conf/supervisor/current.ini")
         sudo("ln -s %(label)s.ini conf/supervisor/current.ini" % env)
 
-        sudo("rm -f celeryconfig.py")
-        sudo("ln -s conf/celery/%(label)s.py celeryconfig.py" % env)
-
-        sudo("rm -f /etc/cron.d/%(label)s" % env)
-        sudo("cp conf/cron/%(label)s.crontab /etc/cron.d/%(label)s" % env)
-
 def reload_processes(reload_type="soft"):
     print(colors.yellow("Reloading processes."))
 
-    env.label = env.host_string.replace("cmb-", "")
+    env.label = env.host_string.replace("%(app)s-", "")
     with cd(env.dest):
-        sudo("supervisorctl -c conf/supervisord.conf reread")
-        sudo("supervisorctl -c conf/supervisord.conf update")
-        # reload gunicorn
-
         sudo("kill -HUP `cat /tmp/gunicorn.%(app)s.%(label)s.pid`" % env)
-        sudo("supervisorctl -c conf/supervisord.conf restart celery:*")
 
 def add_commit_sha():
     repo = git.Repo(".")

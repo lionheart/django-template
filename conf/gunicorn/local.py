031b3e9a389bf105b6ccdce2f4e0fa71e6af3853
Dan Loewenherz <dloewenherz@gmail.com>
2012-11-24 13:42:41 -0800

    update supervisor and gunicorn files

--- conf/gunicorn/local.py
+++ conf/gunicorn/local.py
@@ -1,9 +1,13 @@
-bind = "unix:/tmp/{{ project_name }}.sock"
+def on_starting(server):
+    import sys
+    sys.path.append("..")
+
+bind = "0.0.0.0:8000"
 workers = 3
 daemon = False
-pidfile = "/tmp/{{ project_name }}.pid"
 loglevel = "debug"
 proc_name = "{{ project_name }}"
 worker_class = "gevent"
 debug = True
+django_settings = "{{ project_name }}.settings"
 
--- conf/gunicorn/production_debug.py
+++ conf/gunicorn/production_debug.py
@@ -1,11 +1,20 @@
-bind = "unix:/tmp/{{ project_name }}.sock"
-workers = 3
+def on_starting(server):
+    import sys
+    sys.path.append("..")
+
+bind = "unix:/tmp/gunicorn.{{ project_name }}.production.sock"
+
+# http://gunicorn.org/design.html#how-many-workers
+workers = 5
+
+# Supervisor needs a non-daemonized process
 daemon = False
+
 pidfile = "/tmp/gunicorn.{{ project_name }}.production.pid"
 loglevel = "debug"
-proc_name = "{{ project_name }}"
+proc_name = "{{ project_name }}-debug"
 worker_class = "gevent"
 debug = True
-user = "gunicorn"
-group = "gunicorn"
+
+django_settings = "{{ project_name }}.settings"
 

756f80461a6259f473a25c3f6093f9ee2577d933
Dan Loewenherz <dloewenherz@gmail.com>
2012-11-24 13:42:41 -0800

    update supervisor and gunicorn files

--- conf/gunicorn/production.py
+++ conf/gunicorn/production.py
@@ -1,3 +1,7 @@
+def on_starting(server):
+    import sys
+    sys.path.append("..")
+
 bind = "unix:/tmp/gunicorn.{{ project_name }}.production.sock"
 
 # http://gunicorn.org/design.html#how-many-workers
@@ -12,3 +16,4 @@ proc_name = "{{ project_name }}-production"
 worker_class = "gevent"
 debug = False
 
+django_settings = "{{ project_name }}.settings"

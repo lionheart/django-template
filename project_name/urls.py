4cd0a61f466b6fa6c2d117f07ef674700ec039f0
Dan Loewenherz <dloewenherz@gmail.com>
2013-01-14 21:58:24 -0800

    update static storage serving

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,8 +1,7 @@
-from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 

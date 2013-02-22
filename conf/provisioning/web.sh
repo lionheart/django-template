5f13ae8dd7ec34af4abeca548d3063b6f4ce2c3b
Dan Loewenherz <dloewenherz@gmail.com>
2013-02-22 10:11:09 -0800

    minor provisioning edits

--- conf/provisioning/web.sh
+++ conf/provisioning/web.sh
@@ -8,6 +8,9 @@ yes | add-apt-repository ppa:nginx/stable
 apt-get update
 yes | apt-get install nginx memcached rsync libpq-dev python-dev libxslt1-dev libxml2-dev python-psycopg2 git-core redis-server postgresql-client inetutils-syslogd apache2-utils python-pip libevent-dev libgraphviz-dev python-pip
 
+# Uncomment if you want to install Wordpress
+# yes | apt-get install php5-fpm php5-mysql mysql-server
+
 # Create the www project directory
 mkdir -p /var/www/{{ project_name }}
 chown -R ubuntu:ubuntu /var/www/{{ project_name }}
@@ -19,19 +22,22 @@ mkdir -p /var/log/rotated
 mkdir -p /etc/nginx/ssl
 
 # Replace the default logrotation
-rm /etc/logrotate.conf
+rm -f /etc/logrotate.conf
 ln -s /var/www/{{ project_name }}/conf/logrotate.conf /etc/
 
 # Use a stripped down nginx configuration file
-rm /etc/nginx/nginx.conf
+rm -f /etc/nginx/nginx.conf
 ln -s /var/www/{{ project_name }}/conf/nginx/base.ngx /etc/nginx/nginx.conf
 
 # Remove all existing enabled nginx sites
-rm /etc/nginx/sites-enabled/*
-ln -s /var/www/{{ project_name }}/conf/nginx/current.gunicorn.ngx /etc/nginx/sites-enabled/
+rm -f /etc/nginx/sites-enabled/*
+ln -s /var/www/{{ project_name }}/conf/nginx/$ENVIRONMENT.ngx /etc/nginx/sites-enabled/
 
 pip install virtualenv
 sudo -u ubuntu mkdir /home/ubuntu/environments/{{ project_name }}
 sudo -u ubuntu virtualenv /home/ubuntu/environments/{{ project_name }}
 pip install supervisor
+
+sudo gem install --no-rdoc --no-ri compass
+sudo gem install --no-rdoc --no-ri oily_png
 

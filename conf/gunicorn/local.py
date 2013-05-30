1a2d09829f57540bdfc4124c17c7490fc0fe7ae5
Dan Loewenherz <dloewenherz@gmail.com>
2013-05-30 15:18:45 -0700

    update gunicorn configurations

--- conf/gunicorn/local.py
+++ conf/gunicorn/local.py
@@ -1,14 +1,9 @@
-def on_starting(server):
-    from django.core.management import call_command
-    call_command('syncmedia')
-
-import os
-pythonpath = os.path.abspath("..")
 bind = "0.0.0.0:8000"
 workers = 3
 daemon = False
 loglevel = "debug"
 proc_name = "{{ project_name }}"
+pidfile = "/tmp/{{ project_name }}.pid"
 worker_class = "gevent"
 debug = True
 django_settings = "{{ project_name }}.settings"

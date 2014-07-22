dff594cd377054a6508a2e01ccb779dbcb573da4
Dan Loewenherz <dloewenherz@gmail.com>
2014-07-22 11:51:26 -0500

    more improvements

--- conf/settings/local.py
+++ conf/settings/local.py
@@ -4,8 +4,12 @@ BASE = os.path.abspath(os.path.dirname(__name__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '{{ project_name }}.db'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '{{ project_name }}_local',
+        'USER': '{{ project_name }}_local',
+        'PASSWORD': '{{ project_name }}_local',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
@@ -16,7 +20,18 @@ CACHES = {
     }
 }
 
+RAVEN_CONFIG = {
+    'dsn': ""
+}
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'statictastic.backends.VersionedFileSystemStorage'
+
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+STATIC_URL = "/static/"
+MEDIA_URL = "/uploads/"
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/tmp/app-messages'
 
 BASE_URL = "http://local.{{ project_name }}.com"

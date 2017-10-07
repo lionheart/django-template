8440d9d40fb1653827542b6544de24e9a64afe7b
Dan Loewenherz <dan@lionheartsw.com>
2017-10-06 19:05:16 -0500

    add argon2 password hashing

--- settings.py
+++ settings.py
@@ -45,6 +45,13 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
 
 INSTALLED_APPS = (
     'django.contrib.auth',

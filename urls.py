27c24f03c5e4b941fe5456fc09b9fb85b845c482
Dan Loewenherz <dan@lionheartsw.com>
2018-01-03 08:15:20 -0600

    update urls.py

--- urls.py
+++ urls.py
@@ -1,11 +1,8 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 from django.http import HttpResponse
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from lionheart.utils import simple_url
-from lionheart.utils import template_url
-from lionheart.utils import home_url
 from lionheart.utils import status_204
 
 from app import views
@@ -13,9 +10,9 @@ from app import views
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^204$', status_204),
-    home_url(views.home),
+    path('admin/', include(admin.site.urls)),
+    path('204$', status_204),
+    path('/', views.home),
 ]
 
 urlpatterns += staticfiles_urlpatterns()

12fca013eb75361ef765713644e4340024d994a7
Dan Loewenherz <dan@lionheartsw.com>
2018-04-23 10:15:27 -0500

    handle all subdomains in ALLOWED_HOSTS

--- settings.py
+++ settings.py
@@ -27,8 +27,8 @@ STATIC_ROOT = os.path.join(BASE, "collected")
 SECRET_KEY = '{{ secret_key }}'
 
 ALLOWED_HOSTS = [
-    "local.{{ project_name }}.com",
     "{{ project_name }}.com",
+    ".{{ project_name }}.com",
     "localhost"
 ]
 
